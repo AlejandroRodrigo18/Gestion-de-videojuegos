6493d7ba48cc2df35de9f889a5720da112dbc133
AlejandroRodrigo18 <alejandro.rodrigo4@educa.madrid.org>
2025-11-12 09:15:03 +0100

    Update cadenas.py

--- src/gestor/etapas/cadenas.py
+++ src/gestor/etapas/cadenas.py
@@ -1,13 +1,14 @@
-from tuplas import videojuegos
+from .tuplas import videojuegos
 
-nombre_videojuego = input("Escribe el nombre de un videojuego: ")
-encontrado = False
+def buscar_videojuego():
+    nombre_videojuego = input("Escribe el nombre de un videojuego: ")
+    encontrado = False
 
-for titulo, año, genero in videojuegos:
+    for titulo, año, genero in videojuegos:
         if nombre_videojuego.lower().strip() == titulo.lower().strip():
             print(f"Título: {titulo} | Género: {genero} | Año: {año}")
             encontrado = True
             break
 
-if not encontrado:
-    print("Ese videojuego no está en la lista")
+    if not encontrado:
+        print("Ese videojuego no está en la lista")
